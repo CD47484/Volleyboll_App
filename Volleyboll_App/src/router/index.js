3726273613ddbb6af9000892dcebc21e766f6517
47491 <47491@gymnasium.ax>
2024-04-19 15:06:38 +0300

    fixar admin som inte funka borja om

--- Volleyboll_App/src/router/index.js
+++ Volleyboll_App/src/router/index.js
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import gruppspel from '../views/gruppspel.vue';
 import slutspel from '../views/slutspel.vue';
-import adminapp from '../views/adminapp.vue';
-import TournamentSetup from '../views/TournamentSetup.vue';
-import AdminDropdown from '../components/AdminDropdown.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,22 +14,7 @@ const router = createRouter({
       path: '/slutspel',
       name: 'slutspel',
       component:slutspel
-    },
-    {
-      path: '/adminapp',
-      name: 'adminapp',
-      component:adminapp
-    },
-    {
-      path: '/TournamentSetup',
-      name: 'TournamentSetup',
-      component:TournamentSetup
-    },
-    {
-      path: '/AdminDropdown',
-      name: 'AdminDropdown',
-      component:AdminDropdown
-    },
+    }
   ]
 })
 
