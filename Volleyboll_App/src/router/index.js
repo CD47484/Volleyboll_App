cbdfc0d8affe4db8c4c3ee626dbfcbf85d1fe02e
47491 <47491@gymnasium.ax>
2024-04-29 10:43:28 +0300

    index AdminView

--- Volleyboll_App/src/router/index.js
+++ Volleyboll_App/src/router/index.js
@@ -2,10 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import gruppspel from '../views/gruppspel.vue';
 import slutspel from '../views/slutspel.vue';
 import App from '../../srcadnin/App.vue';
+import AdminView from '../views/AdminView.vue';
 
 const router = createRouter({
   history: createWebHistory('/~47491/Volleyboll'),
   routes: [
+    {
+      path: '/AdminView',
+      name: 'Adminview',
+      component:AdminView
+    },
     {
       path: '/gruppspel',
       name: 'gruppspel',
